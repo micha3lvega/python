b7731a38b73ba640c05ee045c29eabdf3396265a
Michael Vega <micha3lvega@gmail.com>
2024-01-21 18:38:51 -0500

    Debanado de cadenas

---
+++ Cadenas/DebanadoString.py
@@ -0,0 +1,10 @@
+cadena = "Este es un ejemplo para substrings (Debanado de cadenas)"
+
+print(len(cadena))  # Saber el tamaño de la cadena
+print(cadena[2])    # Obtiene lo que hay en ese lugar empezando en '0'
+print(cadena[0:10]) # indexación de cadenas para seleccionar un rango específico de caracteres
+print(cadena[:10])
+print(cadena[10:])  # Desde el espacio 10 hasta el final
+print(cadena[-1])   # Un numero negativo indica que tomara ese valor, pero comenzando la cuenta en la ultima palabra
+
+
