e48f687d3f9b502255cdd6aa24bd8a9a77b832d8
Michael Vega <micha3lvega@gmail.com>
2024-01-21 00:59:42 -0500

    Operadores

---
+++ Operadores/Operators.py
@@ -0,0 +1,31 @@
+sum = 1 + 2
+print(sum)
+
+substrac = 4-1
+print(substrac)
+
+multiplication = 10 * 10
+print(multiplication)
+
+division = 75 / 9
+print(division)
+
+exp = 10 ** 10
+print(exp)
+
+exp = 2 ** 4
+print(exp)
+
+division = 75 // 9 # Division entera
+print(division)
+
+# Toma solo el residuo de la operacion
+residuo = 24 % 2
+print(residuo)
+
+residuo = 23 % 3
+print(residuo)
+
+# Numeros grandes
+max = 222507385850720202000029999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+print(max)
