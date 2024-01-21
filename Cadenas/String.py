b276c062fce522d88fe20262b421bc5010f00d57
Michael Vega <micha3lvega@gmail.com>
2024-01-21 18:15:29 -0500

    String literals

---
+++ Cadenas/String.py
@@ -0,0 +1,30 @@
+cadena1 = "Hola mundo"
+cadena2 = 'Hola mundo'
+cadena3 = 'Hola "mundo"'
+cadena4 = "Esto es un \"ejemplo\" de cadena de texto"
+cadena5 = 'Esto es un \'ejemplo\' de cadena de texto'
+saltoDeLinea = "Parrafo 1\nParrafo 2"
+tabulacion = "Hola\tmundo"
+removerEspacio = "Sin  \bdoble  \bespacio"
+
+print(cadena1)
+print(cadena2)
+print(cadena3)
+print(cadena4)
+print(cadena5)
+print(saltoDeLinea)
+print(tabulacion)
+print(removerEspacio)
+
+"""
+\n: Salto de línea.
+\t: Tabulación.
+\r: Retorno de carro (mueve el cursor al inicio de la línea).
+\b: Retroceso (borra el carácter anterior).
+\\: Representa una barra invertida literal.
+\': Representa una comilla simple literal dentro de una cadena delimitada por comillas simples.
+\": Representa una comilla doble literal dentro de una cadena delimitada por comillas dobles.
+\a: Carácter de alerta (emite un sonido de alerta, si es compatible con el sistema).
+\f: Avance de página.
+\v: Tabulación vertical.
+"""
