4d83e2d025afb2b7bd8fe7ecaa9bbcda0e2fddb0
Michael Vega <micha3lvega@gmail.com>
2024-01-21 18:27:30 -0500

    Operaciones con cadenas

---
+++ Cadenas/Opertators.py
@@ -0,0 +1,13 @@
+
+cadena1 = "Hola"
+cadena2 = "mundo"
+
+print(cadena1 + " " +  cadena2) #Suma
+print(cadena2 * 5) # Multiplicacion "mundomundomundomundomundo"
+print(cadena2, cadena2)
+
+numero = 1
+print(type(numero))
+
+numero = str(numero) # Convierte una variable a string
+print(type(numero))
