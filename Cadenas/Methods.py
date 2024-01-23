cd36583cac5e191a4367857874f1974f5b6a5a1f
Michael Vega <micha3lvega@gmail.com>
2024-01-22 19:15:09 -0500

    Metodos de las cadenas

---
+++ Cadenas/Methods.py
@@ -0,0 +1,10 @@
+upper = "MAYUSCULAS"
+lower = "minusculas";
+name = "michael vega"
+swapcase = "aBcDeFgH"
+
+print(upper.lower()) # Convertir a mayusculas
+print(lower.upper()) # Convertir a minusculas
+print(name.capitalize()) # Convertir la primera palabra en mayuscula
+print(name.title()) # Convertir la primera palabra antes de un espacio en mayusculas
+print(name.swapcase()) # Convertir las mayusculas a minusculas y viceversa
